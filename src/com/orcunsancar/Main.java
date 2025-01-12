5db05e4b44496effc77b452d3f4133273028a217
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2025-01-12 17:31:51 +0300

    Chaining Functions

--- src/com/orcunsancar/Main.java
+++ src/com/orcunsancar/Main.java
@@ -1,26 +1,18 @@
 package com.orcunsancar;
 
-import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(incrementByOne(1));
-        System.out.println(incrementByOneFunc.apply(1));
-
-        List<Integer> integers = List.of(1, 2, 3)
-                .stream()
-                .map(Main::incrementByOne)
-                .collect(Collectors.toList());
-
-        System.out.println(integers);
+        Function<Integer, Integer> combinedFunction = incrementByOneFunc.andThen(doubleFunc);
+        System.out.println(combinedFunction.apply(1));
+        System.out.println(combinedFunction.apply(3));
     }
 
     static Function<Integer, Integer> incrementByOneFunc =
             n -> n + 1;
 
-    static int incrementByOne(int n) {
-        return n + 1;
-    }
+    static Function<Integer, Integer> doubleFunc =
+            n -> n * 2;
+
 }
