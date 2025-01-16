c6ea1cf15afa33acd23bcdfbdd0d2d35db4ae4fb
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2025-01-16 21:43:57 +0300

    Callback example in java using Consumer

--- src/com/orcunsancar/Main.java
+++ src/com/orcunsancar/Main.java
@@ -1,7 +1,45 @@
 package com.orcunsancar;
 
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
-
+        hello("Alex", "Jones", input -> {
+            System.out.println(input + " you must have last name");
+        });
     }
+
+    /*
+     const cb = (firstName) => {
+        console.log(firstName + " you must have last name")
+    }*/
+
+    static Consumer<String> cb = input ->
+            System.out.println(input + " you must have last name");
+
+    static void hello(String firstName,
+                      String lastName,
+                      Consumer<String> callback) {
+        System.out.println(firstName);
+        if (lastName != null) {
+            System.out.println(lastName);
+        } else {
+            callback.accept(firstName);
+        }
+    }
+
+    /*
+    const hello = (firstName, lastName, callback) => {
+        console.log(firstName);
+        if (lastName) {
+            console.log(lastName);
+        } else {
+            callback(firstName);
+        }
+    }
+
+    const cb = (firstName) => {
+        console.log(firstName + " you must have last name")
+    }*/
 }
+
