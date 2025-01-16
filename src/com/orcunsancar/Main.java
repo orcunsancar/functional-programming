2886cf5320b49a1f962dc1cfeb8ebf4ab19241e8
Emin Orçun SANCAR <eorcunsancar@gmail.com>
2025-01-16 21:34:53 +0300

    Callback example in js

--- src/com/orcunsancar/Main.java
+++ src/com/orcunsancar/Main.java
@@ -3,26 +3,5 @@ package com.orcunsancar;
 public class Main {
     public static void main(String[] args) {
 
-        Person jamila = new Person(
-                "Jamila",
-                "+0000000",
-                "jamila@gmail.com"
-        );
-
-        Person alex = new Person(
-                "Alex",
-                "0000000",
-                "alex@gmail.com"
-        );
-
-
-        PersonRegistrationValidator validator =
-                PersonRegistrationValidator
-                        .isEmailTaken()
-                        .and(PersonRegistrationValidator.isEmailValid())
-                        .and(PersonRegistrationValidator.isPhoneNumberValid());
-
-        System.out.println(validator.apply(jamila));
-        System.out.println(validator.apply(alex));
     }
 }
